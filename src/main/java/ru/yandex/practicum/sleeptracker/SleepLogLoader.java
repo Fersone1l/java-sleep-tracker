0a6ea15b3b4709b3e6af43400caba8634e181879
fersoneil <Fersoneil@yandex.ru>
2026-03-02 21:52:01 +0500

    исправил code style

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepLogLoader.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepLogLoader.java
@@ -7,7 +7,6 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
